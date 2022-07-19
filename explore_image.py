cdbf434f8bc2d6b69b24286b3651d661ba0eb649
PatrickTyBrown <patrick.ty.brown@gmail.com>
2022-07-18 23:18:02 -0500

    xplore img to find field coord

--- explore_image.py
+++ explore_image.py
@@ -11,42 +11,23 @@ def click_event(event, x, y, flags, params):
 	# checking for left mouse clicks
 	if event == cv2.EVENT_LBUTTONDOWN:
 
-		# displaying the coordinates
-		# on the Shell
-		print(x, ' ', y)
+		print('{"field": "blank", "x": ' + str(x) + ', "y": ' + str(y) + '}')
 
-		# displaying the coordinates
-		# on the image window
 		font = cv2.FONT_HERSHEY_SIMPLEX
 		cv2.putText(img, str(x) + ',' +
 					str(y), (x,y), font,
 					1, (255, 0, 0), 2)
 		cv2.imshow('image', img)
 
-	# checking for right mouse clicks	
-	if event==cv2.EVENT_RBUTTONDOWN:
-
-		# displaying the coordinates
-		# on the Shell
-		print(x, ' ', y)
-
-		# displaying the coordinates
-		# on the image window
-		font = cv2.FONT_HERSHEY_SIMPLEX
-		b = img[y, x, 0]
-		g = img[y, x, 1]
-		r = img[y, x, 2]
-		cv2.putText(img, str(b) + ',' +
-					str(g) + ',' + str(r),
-					(x,y), font, 1,
-					(255, 255, 0), 2)
-		cv2.imshow('image', img)
 
 # driver function
 if __name__=="__main__":
 
 	# reading the image
-	img = cv2.imread('templates/IncomeDrivenRepayment.pdf', 1)
+	img = cv2.imread('templates_img/IncomeDrivenRepayment_0.jpg', 1)
+
+	#resize image
+	img = cv2.resize(img, (850, 1100))   
 
 	# displaying the image
 	cv2.imshow('image', img)
